74bf056e40c776adbf6856b5fccd316f691304f7
Proto-Pete <peter@protolyst.org>
2022-08-04 09:49:35 +0100

    Button added proptypes and cleaned up

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,14 @@
+import PropTypes from "prop-types";
 import React from "react";
 import styles from "./Button.module.scss";
 
 const Button = (props) => {
    const {handleClick, text} = props;
 
+   Button.propTypes = {
+      handleClick: PropTypes.func.isRequired,
+      text: PropTypes.string
+   };
    return (
       <>
          <button
